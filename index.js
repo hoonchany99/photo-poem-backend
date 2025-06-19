90d641ba63b1b2c459d0d2e733e12d1e88b89ee2
윤훈찬 <hoonchan@yunhunchan-ui-MacBookPro.local>
2025-06-19 21:51:59 +0900

    port change

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const OpenAI = require('openai').OpenAI;
 
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -84,6 +83,7 @@ app.post('/poem', async (req, res) => {
   }
 });
 
+const port = process.env.PORT || 3001;
 app.listen(port, '0.0.0.0', () => {
   console.log(`서버 실행 중: http://localhost:${port}`);
 });
